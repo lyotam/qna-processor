3ff7168b84878564723bbd352f39007b682e68cf
Yotam Leshem <leshemyotam@gmail.com>
2020-08-28 02:19:17 +0300

    Added additional documentation

--- README.md
+++ README.md
@@ -6,8 +6,33 @@ Q&A-Processor is a Kotlin based web server app which, upon client request, extra
 ## Instructions
 
 In order to receive a list of Q&As for a specific Amazon product, you will need to have the product's ASIN.
+You have the option of specifying the amount of Q&As you wish to retrieve (capped to Amazon's 1000 max questions, defaults to 10 if not specified).
+
 You can send the request to the server via the following methods:
 
-#### REST 
+#### REST (POSTMAN, CURL)
+http://127.0.0.1:8080/qna/{PRODUCT_ASIN}?amount={OPTIONAL_NUMBER}
+
+examples:
+- http://127.0.0.1:8080/qna/B07FZ8S74R?amount=25
+- http://127.0.0.1:8080/qna/Tx1HMRPBQIOKRYD
+
 
 #### SWAGGER UI
+[Swagger UI](http://127.0.0.1:8080/swagger-ui.html) is also available to interact with the app
+
+
+## How to run this app:
+First clone the repository and access the directory:
+```
+$ git clone https://github.com/lyotam/qna-processor.git
+$ cd qna-processor
+```
+
+Running the app:
+```
+# To run the app
+$ ./start.sh
+# To shut down the app
+$ ./stop.sh
+```
--- src/main/kotlin/com/rf/qnaProcessor/controller/QnAController.kt
+++ src/main/kotlin/com/rf/qnaProcessor/controller/QnAController.kt
@@ -3,6 +3,9 @@ package com.rf.qnaProcessor.controller
 import com.rf.qnaProcessor.model.QnAEntry
 import com.rf.qnaProcessor.service.QnAService
 import com.rf.qnaProcessor.util.Logging
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
@@ -10,6 +13,16 @@ import org.springframework.web.bind.annotation.*
 class QnAController (
     private val qnaService: QnAService
 ) : Logging {
+
+    @ApiOperation(
+        value = "Retrieves Amazon Product Q&A list by ASIN",
+        notes = "To specify the desired amount of Q&As, include the amount parameter"
+    )
+    @ApiResponses(
+        ApiResponse(code = 200, message = "The request was processed successfully and the Q&A entry list is returned"),
+        ApiResponse(code = 404, message = "The product corresponding with supplied ASIN was not found"),
+        ApiResponse(code = 503, message = "An Error occurred while processing the Q&A request")
+    )
     @GetMapping("/qna/{asin}")
     @ResponseStatus(HttpStatus.OK)
     fun getProductQnAs(@PathVariable asin: String, @RequestParam(required = false) amount: Int?): List<QnAEntry> {
